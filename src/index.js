34c4e511e95304ab2699ea103cdead429546693e
Larissa Campelo Alves <larissacampeloalves@gmail.com>
2021-08-29 22:30:24 -0300

    erro cors

--- src/index.js
+++ src/index.js
@@ -10,18 +10,22 @@ const port = 3000
 mongoose.connect ('mongodb+srv://larissacamp:BE9BF2FdHW8dtMRK@cluster0.pc8w5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { 
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+const corsOpts = {
+  origin: 'https://jobsnet2021.netlify.app',
 
-app.use (cors())
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+app.use (cors(corsOpts))
 app.use(express.json ());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup (swaggerDocs));
 app.use (routes);
-app.listen ('3000', () =>{
+app.listen (process.env.PORT || '3000', () =>{
   console.log('rodando na porta 3000' );
 });
